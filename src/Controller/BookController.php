97fdd685fed0ca75b9dcfa6f12146f48f5120b02
berkaybozkurt <bozkurtberkay@hotmail.com>
2022-02-21 21:49:35 +0300

    Book CRUD operation

---
+++ src/Controller/BookController.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use App\Form\Type\BookType;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/book", name="book_")
+ */
+class BookController extends AbstractApiController
+{
+    /**
+     * @Route("/", name="list", methods={"GET|HEAD"})
+     * @param BookRepository $repository
+     * @return Response
+     */
+    public function list(BookRepository $repository): Response
+    {
+        $books = $repository->findAll();
+
+        return $this->respond($books);
+    }
+
+    /**
+     * @Route("/", name="create", methods={"POST"})
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->buildForm(BookType::class);
+
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->respond($form, Response::HTTP_BAD_REQUEST);
+        }
+
+        /**
+         * @var Book $book
+         */
+        $book = $form->getData();
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $this->respond($book, Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/{id}", name="read", methods={"GET"})
+     * @param int $id
+     * @param BookRepository $repository
+     * @return Response
+     */
+    public function show(int $id, BookRepository $repository): Response
+    {
+        $book = $repository->find($id);
+        if (empty($book)) {
+            return $this->respond(["message" => "Book Not Found"]);
+        }
+
+        return $this->respond($book);
+    }
+
+    /**
+     * @Route("/{id}", name="update", methods={"PUT"})
+     * @param Request $request
+     * @param int $id
+     * @param BookRepository $repository
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function edit(Request $request, int $id, BookRepository $repository, EntityManagerInterface $entityManager): Response
+    {
+        $book = $repository->find($id);
+        if (empty($book)) {
+            return $this->respond(["message" => "Book Not Found"]);
+        }
+
+        $form = $this->buildForm(BookType::class, $book, [
+            'method' => $request->getMethod(),
+        ]);
+
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->respond($form, Response::HTTP_BAD_REQUEST);
+        }
+
+        /** @var Book $book */
+        $book = $form->getData();
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $this->respond($book, Response::HTTP_CREATED);
+
+    }
+
+    /**
+     * @Route("/{id}", name="delete", methods={"DELETE"})
+     * @param int $id
+     * @param BookRepository $repository
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function delete(int $id, BookRepository $repository, EntityManagerInterface $entityManager): Response
+    {
+        $book = $repository->find($id);
+
+        if(empty($book))
+        {
+            return $this->respond(["message" => "Book Not Found"]);
+        }
+
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->respond(["message" => "Author delete successfully", "book" => $book]);
+    }
+}
---
+++ src/Entity/Book.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity(repositoryClass=BookRepository::class)
+ */
+class Book
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var Author|null
+     * @Groups("author_id")
+     * @ORM\ManyToOne(targetEntity=Author::class, inversedBy="books")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?Author
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?Author $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+}
